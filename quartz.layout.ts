f88d42b6226b24eaf7925cab4ebd81f265d5d6fd
luhao <luhao200@foxmail.com>
2026-03-11 10:52:19 +0800

    feat(quartz): ✨️美化

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,11 +5,16 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.RecentNotes({
+      title: "最近更新",
+      limit: 3,
+      showTags: true,
+    }),
+  ],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/luhao200",
     },
   }),
 }
